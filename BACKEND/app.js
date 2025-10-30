88d3f0d8476ae3e7bfb6dde4eb7c4209f22c74c9
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-10-30 12:23:14 +0530

    commit

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -40,7 +40,7 @@ app.get("/:id",redirectFromShortUrl)
 app.use(errorHandler)
 
 // Use Render's dynamic port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Connect DB before starting server
 connectDB();
