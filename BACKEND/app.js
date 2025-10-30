5ceb33a5f7d53ca685d5e118f5fe80d53cf17fc6
Mukesh Choudhary <Mukesh.13672@stu.upes.ac.in>
2025-10-30 11:00:14 +0530

    commit

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -13,7 +13,7 @@ import cookieParser from "cookie-parser"
 
 dotenv.config("./.env")
 
-const app = express();
+const app = express();  
 
 app.use(cors({
     origin: 'http://localhost:5173', // your React app
